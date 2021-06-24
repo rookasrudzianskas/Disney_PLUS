eeb57e7524a42b6df5fbca5b6e5cb843a822a35a
Rokas <rookas.rudzenskas@gmail.com>
2021-06-24 10:06:35 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            Header
+        </Container>
     );
 };
 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyApJx_SkmoKCOqWxnJi-MK3Qm9hJp6v1kI",
+    authDomain: "rookas-disney-plus.firebaseapp.com",
+    projectId: "rookas-disney-plus",
+    storageBucket: "rookas-disney-plus.appspot.com",
+    messagingSenderId: "533750515529",
+    appId: "1:533750515529:web:ebd889e3408882ab643c98",
+    measurementId: "G-H60YBNBQJ8"
+};
+
