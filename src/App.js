566d7822bd4cc1b8319de74cfe2f47c6ca269e57
Rokas <rookas.rudzenskas@gmail.com>
2021-06-24 09:56:43 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Disney Plus 🎤</h1>
     </div>
   );
 }
