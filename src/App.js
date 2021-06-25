17b9adde5e5733822bfce97c65b4074687167a6a
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:37:18 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Movies from "./components/Movies";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Home />
+        <Movies />
     </div>
   );
 }
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -67,7 +67,7 @@ const Wrap = styled.div`
       height: 100%;
       border-radius: 4px;
       box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-      transition: all 0.2s;
+      transition: all 0.25s;
       
       &:hover {
         border: 4px solid rgba(249, 249, 249, 0.8);
