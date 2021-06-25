8f2533bc7665bc7b15c903d5f4b7a96dfbd33408
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 08:51:26 +0300

    Rokas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 const Home = () => {
     return (
         <Container>
-            Home
+            <ImgSlider />
         </Container>
     );
 };
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ImgSlider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ImgSlider;
