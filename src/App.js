0fd697d5045ebb61b79edc35a184b777d5e1093d
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 10:12:18 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
