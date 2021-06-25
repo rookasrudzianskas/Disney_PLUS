3f027f73812f63c0b26fbf6bc3180337d6a2680b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 10:19:13 +0300

    Rokas

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 
 const Detail = () => {
     return (
-        <div>
+        <Container>
 
-        </div>
+        </Container>
     );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+`
