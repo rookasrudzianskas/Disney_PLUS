b6c8527b130b88918f539884f184fbb30521a8f7
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 16:37:57 +0300

    Rokas

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counterSlice';
 import movieReducer from '../features/movie/movieSlice';
+import userReducer from '../features/user/userReducer';
 
 export const store = configureStore({
   reducer: {
     movie: movieReducer,
+    user: userReducer,
   },
 });
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -26,6 +26,8 @@ const userSlice = createSlice({
 
 export const { setUserLogin, setSignOut } = userSlice.actions;
 
-export const select = (state) => state.user.movies;
+export const selectUserName = (state) => state.user.name;
+export const selectUserEmail = (state) => state.user.email;
+export const selectUserPhoto = (state) => state.user.photo;
 
 export default userSlice.reducer;
