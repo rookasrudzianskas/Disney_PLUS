ba70355f3fad87d39b59be7653ef277d40724458
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:33:23 +0300

    Rokas

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -42,10 +42,18 @@ const Container = styled.div`
 const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   border-radius: 10px;
+  box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
   
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
     }
+  
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    transition: all 0.2s;
+  }
 `
