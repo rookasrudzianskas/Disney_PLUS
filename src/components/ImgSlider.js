2c6831493adfea06c0d566051449b183c7d84c01
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 08:59:25 +0300

    Rokas

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const ImgSlider = () => {
+
+    // settings for the slider
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
+
+
     return (
-        <div>
+        // this puts all the settings, straight to the carousel component
+        <Carousel {...settings}>
             
-        </div>
+        </Carousel>
     );
 };
 
 export default ImgSlider;
+
+const Carousel = styled(Slider)`
+  
+`
