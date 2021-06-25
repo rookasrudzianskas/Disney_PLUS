a7aadc012756fb5e6af0636d4abd77322246987a
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:21:01 +0300

    Rokas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 const Home = () => {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     );
 };
