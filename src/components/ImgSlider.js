e454948676768f83778e20124d0e42b78de76909
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:07:14 +0300

    Rokas

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -21,7 +21,14 @@ const ImgSlider = () => {
     return (
         // this puts all the settings, straight to the carousel component
         <Carousel {...settings}>
-            
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt=""/>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt=""/>
+            </Wrap>
+
         </Carousel>
     );
 };
@@ -29,5 +36,20 @@ const ImgSlider = () => {
 export default ImgSlider;
 
 const Carousel = styled(Slider)`
+  margin-top: 20px;
   
+  .slick-list {
+    overflow: visible;
+  }
+`
+
+const Wrap = styled.div`
+  
+    
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 4px;
+      box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    }
 `
