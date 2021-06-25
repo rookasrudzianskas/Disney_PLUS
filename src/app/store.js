aa47267dbc1539931f454b434087b2f37ad2acd6
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 15:36:38 +0300

    Rokas

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counterSlice';
+import movieReducer from '../features/movie/movieSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    movie: movieReducer,
   },
 });
