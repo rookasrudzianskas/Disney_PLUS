b64919cd8d8c32d89147c168a02b58e2e382cc5a
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 15:15:11 +0300

    Rokas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
 
 const Home = () => {
+
+    useEffect(() => {
+
+    }, []);
+
+
     return (
         <Container>
             <ImgSlider />
