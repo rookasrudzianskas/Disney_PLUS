a1b7cc71b4c969c45458d8b0553a03136bff8bf0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:49:57 +0300

    Rokas

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -44,10 +44,12 @@ const Content = styled.div`
 const Wrap = styled.div`
   border-radius: 10px;
   overflow: hidden;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0, 0, 0 /69%) 0px 26px 30px -10px, rgb(0, 0, 0 / 73%) 0px 16px 10px -10px;
     
     img {
       width: 100%;
       height: 100%;
-      object-fit: contain;
+      object-fit: cover;
     }
 `
