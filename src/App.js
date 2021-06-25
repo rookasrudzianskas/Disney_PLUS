87b48e42d26eb3db5249798cfbcbbe6018ce0546
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:42:35 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Movies from "./components/Movies";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Home />
-        <Movies />
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,10 +3,29 @@ import styled from "styled-components";
 
 const Movies = () => {
     return (
-        <div>
+        <Container>
+            <h4>Recommended For You</h4>
 
-        </div>
+            <Content>
+                <Wrap>
+                    <img src="https://www.charlotteruff.com/wp-content/uploads/2020/04/disney-classics-1-1600x800.jpg" alt=""/>
+                </Wrap>
+            </Content>
+        </Container>
     );
 };
 
 export default Movies;
+
+
+const Container = styled.div`
+  
+`
+
+const Content = styled.div`
+  
+`
+
+const Wrap = styled.div`
+  
+`
