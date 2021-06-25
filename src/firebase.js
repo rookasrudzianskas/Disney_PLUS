9d652d91b05f7b89ca4249982c22dec1fc20669c
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 15:09:31 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -11,3 +11,12 @@ const firebaseConfig = {
     measurementId: "G-H60YBNBQJ8"
 };
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+//     authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+//     projectId: "disneyplus-clone-a33d5",
+//     storageBucket: "disneyplus-clone-a33d5.appspot.com",
+//     messagingSenderId: "37918794208",
+//     appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+//     measurementId: "G-DRVLJKWRWG",
+// };
