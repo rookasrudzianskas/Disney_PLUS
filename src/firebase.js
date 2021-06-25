b687abd92972604677e2765a763e5e8e6e2201cd
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 15:10:20 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,25 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// my database
+// const firebaseConfig = {
+//     apiKey: "AIzaSyApJx_SkmoKCOqWxnJi-MK3Qm9hJp6v1kI",
+//     authDomain: "rookas-disney-plus.firebaseapp.com",
+//     projectId: "rookas-disney-plus",
+//     storageBucket: "rookas-disney-plus.appspot.com",
+//     messagingSenderId: "533750515529",
+//     appId: "1:533750515529:web:ebd889e3408882ab643c98",
+//     measurementId: "G-H60YBNBQJ8"
+// };
+
+// working one
 const firebaseConfig = {
-    apiKey: "AIzaSyApJx_SkmoKCOqWxnJi-MK3Qm9hJp6v1kI",
-    authDomain: "rookas-disney-plus.firebaseapp.com",
-    projectId: "rookas-disney-plus",
-    storageBucket: "rookas-disney-plus.appspot.com",
-    messagingSenderId: "533750515529",
-    appId: "1:533750515529:web:ebd889e3408882ab643c98",
-    measurementId: "G-H60YBNBQJ8"
+    apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+    authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+    projectId: "disneyplus-clone-a33d5",
+    storageBucket: "disneyplus-clone-a33d5.appspot.com",
+    messagingSenderId: "37918794208",
+    appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+    measurementId: "G-DRVLJKWRWG",
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-//     authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-//     projectId: "disneyplus-clone-a33d5",
-//     storageBucket: "disneyplus-clone-a33d5.appspot.com",
-//     messagingSenderId: "37918794208",
-//     appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-//     measurementId: "G-DRVLJKWRWG",
-// };
