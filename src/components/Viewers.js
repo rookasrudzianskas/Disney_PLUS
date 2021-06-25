5679a29f5acff8dd3666f2b07f8dfeae998fb1df
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 09:29:47 +0300

    Rokas

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import styled from "styled-components";
 
-
 const Viewers = () => {
     return (
-        <div>
+        <Container>
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt=""/>
+            </Wrap>
 
-        </div>
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt=""/>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt=""/>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt=""/>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt=""/>
+            </Wrap>
+
+        </Container>
     );
 };
 
 export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  display: grid;
+  // create 5 columns, stretch out
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-gap: 25px;
+  padding: 30px 0 26px;
+`
+
+const Wrap = styled.div`
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  border-radius: 10px;
+  
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+`
